aea11722a61fe50426cadd092ff6d81f55727c6b
Krishna Chamarthy <chamarthysr@gmail.com>
2026-05-29 21:46:33 +0530

    init

---
+++ SwiftSync/SwiftSync/SwiftSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSyncApp.swift
+//  SwiftSync
+//
+//  Created by Krishna Chamarthy on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
